71e74f2d748fbe5f1a8cbf4018e7ffd0dfeeade3
h225liu <h225liu@student.cs.uwaterloo.ca>
2016-01-11 22:13:39 -0500

    first commit, adding PrefixCount

--- src/main/java/ca/uwaterloo/cs/bigdata2016w/evisoup/assignment0/PrefixCount.java
+++ src/main/java/ca/uwaterloo/cs/bigdata2016w/evisoup/assignment0/PrefixCount.java
@@ -30,8 +30,8 @@ import org.kohsuke.args4j.ParserProperties;
 /**
  * Simple word count demo.
  */
-public class WordCount extends Configured implements Tool {
-  private static final Logger LOG = Logger.getLogger(WordCount.class);
+public class PrefixCount extends Configured implements Tool {
+  private static final Logger LOG = Logger.getLogger(PrefixCount.class);
 
   // Mapper: emits (token, 1) for every word occurrence.
   private static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
@@ -47,8 +47,11 @@ public class WordCount extends Configured implements Tool {
       while (itr.hasMoreTokens()) {
         String w = itr.nextToken().toLowerCase().replaceAll("(^[^a-z]+|[^a-z]+$)", "");
         if (w.length() == 0) continue;
-        WORD.set(w);
-        context.write(WORD, ONE);
+        if (w.length() > 1 ){
+          WORD.set(w.substring(0, 2));
+          context.write(WORD, ONE);
+        }
+        
       }
     }
   }
@@ -65,11 +68,14 @@ public class WordCount extends Configured implements Tool {
         String w = itr.nextToken().toLowerCase().replaceAll("(^[^a-z]+|[^a-z]+$)", "");
         if (w.length() == 0) continue;
 
-        if (counts.containsKey(w)) {
-          counts.put(w, counts.get(w)+1);
-        } else {
-          counts.put(w, 1);
+        if (w.length() > 1 ){
+          if (counts.containsKey(w.substring(0, 2))) {
+            counts.put(w.substring(0, 2), counts.get(w.substring(0, 2))+1);
+          } else {
+            counts.put(w.substring(0, 2), 1);
+          }
         }
+        
       }
     }
 
@@ -108,7 +114,7 @@ public class WordCount extends Configured implements Tool {
   /**
    * Creates an instance of this tool.
    */
-  public WordCount() {}
+  public PrefixCount() {}
 
   public static class Args {
     @Option(name = "-input", metaVar = "[path]", required = true, usage = "input path")
@@ -139,7 +145,7 @@ public class WordCount extends Configured implements Tool {
       return -1;
     }
 
-    LOG.info("Tool: " + WordCount.class.getSimpleName());
+    LOG.info("Tool: " + PrefixCount.class.getSimpleName());
     LOG.info(" - input path: " + args.input);
     LOG.info(" - output path: " + args.output);
     LOG.info(" - number of reducers: " + args.numReducers);
@@ -147,8 +153,8 @@ public class WordCount extends Configured implements Tool {
 
     Configuration conf = getConf();
     Job job = Job.getInstance(conf);
-    job.setJobName(WordCount.class.getSimpleName());
-    job.setJarByClass(WordCount.class);
+    job.setJobName(PrefixCount.class.getSimpleName());
+    job.setJarByClass(PrefixCount.class);
 
     job.setNumReduceTasks(args.numReducers);
 
@@ -180,6 +186,6 @@ public class WordCount extends Configured implements Tool {
    * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
    */
   public static void main(String[] args) throws Exception {
-    ToolRunner.run(new WordCount(), args);
+    ToolRunner.run(new PrefixCount(), args);
   }
 }
