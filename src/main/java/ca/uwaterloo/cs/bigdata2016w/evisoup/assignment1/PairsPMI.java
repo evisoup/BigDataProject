df3f937d3bdf6f93e7ecf9b8b624112236786ed2
h225liu <h225liu@student.cs.uwaterloo.ca>
2016-01-19 05:39:26 -0500

    adding config

--- src/main/java/ca/uwaterloo/cs/bigdata2016w/evisoup/assignment1/PairsPMI.java
+++ src/main/java/ca/uwaterloo/cs/bigdata2016w/evisoup/assignment1/PairsPMI.java
@@ -299,6 +299,12 @@ protected static class MyReducer extends
     FileInputFormat.setInputPaths(job, new Path(args.input));
     FileOutputFormat.setOutputPath(job, new Path(args.output));
 
+    job.getConfiguration().setInt("mapred.max.split.size", 1024 * 1024 * 64);
+    job.getConfiguration().set("mapreduce.map.memory.mb", "3072");
+    job.getConfiguration().set("mapreduce.map.java.opts", "-Xmx3072m");
+    job.getConfiguration().set("mapreduce.reduce.memory.mb", "3072");
+    job.getConfiguration().set("mapreduce.reduce.java.opts", "-Xmx3072m");
+
     job.setMapOutputKeyClass(PairOfStrings.class);
     job.setMapOutputValueClass(FloatWritable.class);
     job.setOutputKeyClass(PairOfStrings.class);
