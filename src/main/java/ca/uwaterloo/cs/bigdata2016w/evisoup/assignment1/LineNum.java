1a094682ed3a4aec4639cfedcf3a316173938fa3
h225liu <h225liu@student.cs.uwaterloo.ca>
2016-01-19 01:00:14 -0500

    add line code & result, finished pair

---
+++ src/main/java/ca/uwaterloo/cs/bigdata2016w/evisoup/assignment1/LineNum.java
@@ -0,0 +1,194 @@
+package ca.uwaterloo.cs.bigdata2016w.evisoup.assignment1;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.HashSet;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
+
+import tl.lin.data.pair.PairOfStrings;
+
+
+/**
+ * Counts bigrams.
+ */
+public class LineNum extends Configured implements Tool {
+  private static final Logger LOG = Logger.getLogger(LineNum.class);
+
+  protected static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private static final IntWritable ONE = new IntWritable(1);
+    private static final Text BIGRAM = new Text();
+
+    @Override
+    public void map(LongWritable key, Text value, Context context)
+        throws IOException, InterruptedException {
+      String line = ((Text) value).toString();
+      StringTokenizer itr = new StringTokenizer(line);
+
+      int cnt = 0;
+      //Set set = Sets.newHashSet();
+      HashSet<String> set = new HashSet<String>();
+      while (itr.hasMoreTokens()) {
+        cnt++;
+        String w = itr.nextToken().toLowerCase().replaceAll("(^[^a-z]+|[^a-z]+$)", "");
+        if (w.length() == 0) continue;
+        set.add(w);
+        if (cnt >= 100) break;
+      }
+
+      String[] words = new String[set.size()];
+      words = set.toArray(words);
+
+      // Your code goes here...
+      BIGRAM.set("TotalLine");
+      context.write(BIGRAM,ONE);
+
+      for (int i = 0; i < words.length; ++i ) {
+
+        //p(x)
+        String left = words[i];
+        BIGRAM.set(left);
+        context.write(BIGRAM,ONE);
+      }
+   }
+
+  }
+
+  protected static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    private final static IntWritable SUM = new IntWritable();
+
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException,
+        InterruptedException {
+      int sum = 0;
+      Iterator<IntWritable> iter = values.iterator();
+      while (iter.hasNext()) {
+        sum += iter.next().get();
+      }
+      SUM.set(sum);
+      context.write(key, SUM);
+    }
+  }
+
+  /**
+   * Creates an instance of this tool.
+   */
+  public LineNum() {}
+
+  public static class Args {
+    @Option(name = "-input", metaVar = "[path]", required = true, usage = "input path")
+    public String input;
+
+    @Option(name = "-output", metaVar = "[path]", required = true, usage = "output path")
+    public String output;
+
+    @Option(name = "-reducers", metaVar = "[num]", required = false, usage = "number of reducers")
+    public int numReducers = 1;
+  }
+
+  /**
+   * Runs this tool.
+   */
+  public int run(String[] argv) throws Exception {
+    Args args = new Args();
+    CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(100));
+
+    try {
+      parser.parseArgument(argv);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      return -1;
+    }
+
+    LOG.info("Tool name: " + LineNum.class.getSimpleName());
+    LOG.info(" - input path: " + args.input);
+    LOG.info(" - output path: " + args.output);
+    LOG.info(" - num reducers: " + args.numReducers);
+
+    Job job = Job.getInstance(getConf());
+    job.setJobName(LineNum.class.getSimpleName());
+    job.setJarByClass(LineNum.class);
+
+    job.setNumReduceTasks(args.numReducers);
+
+    FileInputFormat.setInputPaths(job, new Path(args.input));
+    FileOutputFormat.setOutputPath(job, new Path(args.output));
+
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(IntWritable.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+
+    job.setMapperClass(MyMapper.class);
+    job.setCombinerClass(MyReducer.class);
+    job.setReducerClass(MyReducer.class);
+
+    // Delete the output directory if it exists already.
+    Path outputDir = new Path(args.output);
+    FileSystem.get(getConf()).delete(outputDir, true);
+
+    long startTime = System.currentTimeMillis();
+    job.waitForCompletion(true);
+    System.out.println("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
+
+    return 0;
+  }
+
+  /**
+   * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
+   */
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new LineNum(), args);
+  }
+}
